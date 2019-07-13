98095762c12239398832e311ffd5bbb6b65cb788
Kavudhami <abiandabi92@gmail.com>
2019-07-13 17:08:34 -0500

    Write reversed string to file in FASTA compliant

--- ReverseAbridged/src/reverseabridged/ReverseAbridged.java
+++ ReverseAbridged/src/reverseabridged/ReverseAbridged.java
@@ -1,13 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package reverseabridged;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,12 +14,61 @@ import java.util.List;
  *
  * @author abiaps
  */
-public class ReverseAbridged {
+public class ReverseAbridged 
+{
+    private void createAbridgedFasta(List<String> seq)
+    {
+        int count = 1;
+        BufferedWriter bufferedWriter = null;
+        try 
+        {
+            File myFile = new File("C:\\Users\\abiaps\\Downloads\\dummyout.fasta");
+            if (!myFile.exists()) 
+            {
+                myFile.createNewFile();
+            }
+            Writer writer = new FileWriter(myFile);
+            bufferedWriter = new BufferedWriter(writer);
+            StringBuilder fullStr = new StringBuilder();
+            for (String seq1 : seq) 
+            {
+                fullStr.append(">REV_").append(count).append(" reversed\n");
+                if(seq1.length() > 80)
+                {
+                   int end = 80, st = 0;
+                   while(end < seq1.length())
+                   {
+                       fullStr.append(seq1.substring(st, end)).append("\n");
+                       st = end;
+                       end += 80;
+                   }
+                   if(st < seq1.length())
+                   {
+                       fullStr.append(seq1.substring(st)).append("\n");
+                   }                       
+                }
+                count++;
+            }
+            bufferedWriter.write(fullStr.toString());
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                if(bufferedWriter != null) bufferedWriter.close();
+            } 
+            catch(Exception ex){                 
+            }
+        }
+    }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        ReverseAbridged rab = new ReverseAbridged();
         String file = "C:\\Users\\abiaps\\Downloads\\dummy.fasta";
 	try
         {            
@@ -54,8 +102,8 @@ public class ReverseAbridged {
                     seq.add(sb.reverse().toString());
                 }                        
             }
-            System.out.println(" seq size: " + seq.size());
-            
+            // write to file
+            rab.createAbridgedFasta(seq);
       }
       catch(IOException e)
       {
